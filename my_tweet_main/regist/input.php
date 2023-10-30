7a3275da76e016016ea84957b2bbe7669281f35f
sr0427 <220029re@yse-c.net>
2023-10-30 14:04:19 +0900

    upload

--- my_tweet_main/regist/input.php
+++ my_tweet_main/regist/input.php
@@ -1,21 +1,23 @@
 <?php
-//セッション開始
-session_start();
-session_regenerate_id(true);
-// var_dump($_SESSION['regist']);
+require_once('../app.php');
+
 if (!empty($_SESSION['regist'])) {
     $regist = $_SESSION['regist'];
+}
+
+//セッションにエラーがあれば取得
+if (!empty($_SESSION['errors'])) {
+    //セッションのエラー削除
+    $errors = $_SESSION['errors'];
+    unset($_SESSION['errors']);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Tweet</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
+
+<?php include('../app/views/components/head.php') ?>
+
 <body>
     <main id="id" class="d-flex justify-content-center">
         <div class="w-50 mt-3 p-5 bg-light">
@@ -24,14 +26,17 @@ if (!empty($_SESSION['regist'])) {
                 <div class="form-floating mb-2">
                     <input type="text" name="name" value="<?= @$regist['name'] ?>" class="form-control">
                     <label for="" class="form-label">Name</label>
+                    <p class="text-danger"><?= @$errors['name'] ?></p>
                 </div>
                 <div class="form-floating mb-2">
                     <input type="email" name="email" value="<?= @$regist['email'] ?>" class="form-control">
                     <label for="" class="form-label">Email</label>
+                    <p class="text-danger"><?= @$errors['email'] ?></p>
                 </div>
                 <div class="form-floating mb-2">
                     <input type="password" name="password" class="form-control">
                     <label for="" class="form-label">Password</label>
+                    <p class="text-danger"><?= @$errors['password'] ?></p>
                 </div>
                 <div>
                     <button class="w-100 mb-2 btn btn-primary">Regist</button>
